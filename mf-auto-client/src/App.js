628faaec7603fe3d7087d0909dde388b6db92e7e
Hadim FAAL NJIE <lshhf4@lshtm.ac.uk>
2025-05-06 20:50:34 +0000

    CI-false

--- mf-auto-client/src/App.js
+++ mf-auto-client/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { UserProvider } from './Context/UserContext';
-import Admin from './layouts/Admin';
-import Auth from './layouts/Auth';
-import ProtectedRoute from './components/ProtectedRoute';
-import Unauthorized from './components/Unauthorized';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { UserProvider } from "./Context/UserContext";
+import Admin from "./layouts/Admin";
+import Auth from "./layouts/Auth";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Unauthorized from "./components/Unauthorized";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const basename = '';        
+  const basename = process.env.PUBLIC_URL || "";
+  
   return (
     <Router basename={basename}>
       <UserProvider>
